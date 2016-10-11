c7316424fa9c11236cc00718c792cd8b6520d76f
Alex Church <alex@church.id.au>
2016-10-11 20:35:02 +1100

    Enabled importing pp() using from django_goodies import pp.

--- django_goodies/__init__.py
+++ django_goodies/__init__.py
@@ -3,3 +3,4 @@ __version__ = '0.5.0a'
 # Shortcuts to goodies
 from django_goodies.misc import AjaxResponse, get_page  # NOQA
 from django_goodies.utils.mon import M, Mon, mon  # NOQA
+from django_goodies.utils.inspect import pp  # NOQA
