188f8ff3e9e6e231e88e47b7bc17965235893882
Alex Church <alex@church.id.au>
2018-01-15 21:30:48 +1100

    Added MessagingRequestFactory helper class and updated AjaxResponse tests to use it over the full Django test client.

--- djem/tests/test_ajax.py
+++ djem/tests/test_ajax.py
@@ -1,8 +1,17 @@
-from django.urls import reverse
+import json
+
+from django.contrib import messages
 from django.test import TestCase
+
+from djem.ajax import AjaxResponse
+from djem.utils.tests import MessagingRequestFactory
 
 
 class AjaxResponseTestCase(TestCase):
+    
+    def setUp(self):
+        
+        self.factory = MessagingRequestFactory()
     
     def test_response__no_args(self):
         """
@@ -10,8 +19,14 @@ class AjaxResponseTestCase(TestCase):
         with no arguments - the "request" argument is required.
         """
         
+        def view(r):
+            
+            return AjaxResponse()
+        
+        request = self.factory.get('/test/')
+        
         with self.assertRaises(TypeError):
-            self.client.get(reverse('ajax__no_args'))
+            view(request)
     
     def test_response__request_only(self):
         """
@@ -19,10 +34,15 @@ class AjaxResponseTestCase(TestCase):
         "request" argument, and simply contains an empty response body.
         """
         
-        response = self.client.get(reverse('ajax__request_only'))
+        def view(r):
+            
+            return AjaxResponse(r)
         
-        self.assertEquals(response.status_code, 200)
-        self.assertEquals(response.json(), {})
+        request = self.factory.get('/test/')
+        response = view(request)
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.content.decode()), {})
     
     def test_response__data(self):
         """
@@ -30,13 +50,18 @@ class AjaxResponseTestCase(TestCase):
         body.
         """
         
-        response = self.client.get(reverse('ajax__data'))
+        def view(r):
+            
+            return AjaxResponse(r, {'test': 'test'})
         
-        self.assertEquals(response.status_code, 200)
+        request = self.factory.get('/test/')
+        response = view(request)
         
-        data = response.json()
-        self.assertEquals(len(data), 1)
-        self.assertEquals(data['test'], 'test')
+        self.assertEqual(response.status_code, 200)
+        
+        data = json.loads(response.content.decode())
+        self.assertEqual(len(data), 1)
+        self.assertEqual(data['test'], 'test')
     
     def test_response__bad_data(self):
         """
@@ -44,8 +69,14 @@ class AjaxResponseTestCase(TestCase):
         with a non-dict "data" argument.
         """
         
+        def view(r):
+            
+            return AjaxResponse(r, ['test', 'test'])
+        
+        request = self.factory.get('/test/')
+        
         with self.assertRaises(TypeError):
-            self.client.get(reverse('ajax__bad_data'))
+            view(request)
     
     def test_response__success__true(self):
         """
@@ -53,12 +84,17 @@ class AjaxResponseTestCase(TestCase):
         body when no other data is given.
         """
         
-        response = self.client.get(reverse('ajax__success__true'))
+        def view(r):
+            
+            return AjaxResponse(r, success=True)
         
-        self.assertEquals(response.status_code, 200)
+        request = self.factory.get('/test/')
+        response = view(request)
         
-        data = response.json()
-        self.assertEquals(len(data), 1)
+        self.assertEqual(response.status_code, 200)
+        
+        data = json.loads(response.content.decode())
+        self.assertEqual(len(data), 1)
         self.assertIs(data['success'], True)
     
     def test_response__success__false(self):
@@ -67,12 +103,17 @@ class AjaxResponseTestCase(TestCase):
         body.
         """
         
-        response = self.client.get(reverse('ajax__success__false'))
+        def view(r):
+            
+            return AjaxResponse(r, success=False)
         
-        self.assertEquals(response.status_code, 200)
+        request = self.factory.get('/test/')
+        response = view(request)
         
-        data = response.json()
-        self.assertEquals(len(data), 1)
+        self.assertEqual(response.status_code, 200)
+        
+        data = json.loads(response.content.decode())
+        self.assertEqual(len(data), 1)
         self.assertIs(data['success'], False)
     
     def test_response__success__dumb(self):
@@ -81,12 +122,17 @@ class AjaxResponseTestCase(TestCase):
         response body when the success argument is given as a non-boolean value.
         """
         
-        response = self.client.get(reverse('ajax__success__dumb'))
+        def view(r):
+            
+            return AjaxResponse(r, success='dumb')
         
-        self.assertEquals(response.status_code, 200)
+        request = self.factory.get('/test/')
+        response = view(request)
         
-        data = response.json()
-        self.assertEquals(len(data), 1)
+        self.assertEqual(response.status_code, 200)
+        
+        data = json.loads(response.content.decode())
+        self.assertEqual(len(data), 1)
         self.assertIs(data['success'], True)
     
     def test_response__success__data(self):
@@ -95,13 +141,18 @@ class AjaxResponseTestCase(TestCase):
         response body when other data is also provided.
         """
         
-        response = self.client.get(reverse('ajax__success__data'))
+        def view(r):
+            
+            return AjaxResponse(r, {'test': 'test'}, success=True)
         
-        self.assertEquals(response.status_code, 200)
+        request = self.factory.get('/test/')
+        response = view(request)
         
-        data = response.json()
-        self.assertEquals(len(data), 2)
-        self.assertEquals(data['test'], 'test')
+        self.assertEqual(response.status_code, 200)
+        
+        data = json.loads(response.content.decode())
+        self.assertEqual(len(data), 2)
+        self.assertEqual(data['test'], 'test')
         self.assertIs(data['success'], True)
     
     def test_response__messages(self):
@@ -110,25 +161,34 @@ class AjaxResponseTestCase(TestCase):
         messages framework in the response body.
         """
         
-        response = self.client.get(reverse('ajax__messages'))
+        def view(r):
+            
+            messages.success(request, 'This is a success message.')
+            messages.error(request, 'This is an error message.')
+            messages.add_message(request, messages.INFO, 'This is an info message.', extra_tags='special')
+            
+            return AjaxResponse(r)
         
-        self.assertEquals(response.status_code, 200)
+        request = self.factory.get('/test/')
+        response = view(request)
         
-        data = response.json()
-        self.assertEquals(len(data), 1)
-        self.assertEquals(len(data['messages']), 3)
+        self.assertEqual(response.status_code, 200)
+        
+        data = json.loads(response.content.decode())
+        self.assertEqual(len(data), 1)
+        self.assertEqual(len(data['messages']), 3)
         
         success = data['messages'][0]
-        self.assertEquals(success['message'], 'This is a success message.')
-        self.assertEquals(success['tags'], 'success')
+        self.assertEqual(success['message'], 'This is a success message.')
+        self.assertEqual(success['tags'], 'success')
         
         error = data['messages'][1]
-        self.assertEquals(error['message'], 'This is an error message.')
-        self.assertEquals(error['tags'], 'error')
+        self.assertEqual(error['message'], 'This is an error message.')
+        self.assertEqual(error['tags'], 'error')
         
         info = data['messages'][2]
-        self.assertEquals(info['message'], 'This is an info message.')
-        self.assertEquals(info['tags'], 'special info')
+        self.assertEqual(info['message'], 'This is an info message.')
+        self.assertEqual(info['tags'], 'special info')
     
     def test_response__messages__data(self):
         """
@@ -136,15 +196,22 @@ class AjaxResponseTestCase(TestCase):
         messages framework in the response body when other data is also provided.
         """
         
-        response = self.client.get(reverse('ajax__messages__data'))
+        def view(r):
+            
+            messages.success(request, 'This is a success message.')
+            
+            return AjaxResponse(r, {'test': 'test'})
         
-        self.assertEquals(response.status_code, 200)
+        request = self.factory.get('/test/')
+        response = view(request)
         
-        data = response.json()
-        self.assertEquals(len(data), 2)
-        self.assertEquals(len(data['messages']), 1)
-        self.assertEquals(data['test'], 'test')
+        self.assertEqual(response.status_code, 200)
+        
+        data = json.loads(response.content.decode())
+        self.assertEqual(len(data), 2)
+        self.assertEqual(len(data['messages']), 1)
+        self.assertEqual(data['test'], 'test')
         
         success = data['messages'][0]
-        self.assertEquals(success['message'], 'This is a success message.')
-        self.assertEquals(success['tags'], 'success')
+        self.assertEqual(success['message'], 'This is a success message.')
+        self.assertEqual(success['tags'], 'success')
---
+++ djem/utils/tests.py
@@ -0,0 +1,17 @@
+from django.test import RequestFactory
+from django.contrib.messages.storage.fallback import FallbackStorage
+
+
+class MessagingRequestFactory(RequestFactory):
+    
+    def request(self, **request):
+        
+        request = super(MessagingRequestFactory, self).request(**request)
+        
+        # Patch the request so it appears as though the
+        # django.contrib.messages.middleware.MessageMiddleware is in use (as
+        # documented in https://code.djangoproject.com/ticket/17971)
+        request.session = 'session'
+        request._messages = FallbackStorage(request)
+        
+        return request
