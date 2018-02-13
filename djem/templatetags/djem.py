9ee57f9f329adf0023d90ae52bcf9f0861a1e7e9
Alex Church <alex@church.id.au>
2018-02-14 07:52:30 +1100

    Added form_field and checkbox template tags, plus tests.

--- djem/templatetags/djem.py
+++ djem/templatetags/djem.py
@@ -1,7 +1,10 @@
+from django.conf import settings
 from django.template import Library, loader
-from django.template.base import Node, NodeList, TemplateSyntaxError
+from django.template.base import Node, NodeList, TemplateSyntaxError, token_kwargs
 
 register = Library()
+
+DEFAULT_FORM_FIELD_TAG = 'div'
 
 
 #
@@ -122,3 +125,129 @@ def paginate(page):
     }
     
     return context
+
+
+def _transform_kwargs(kwargs):
+    """
+    Replace underscores in the given dictionary's keys with dashes. Used to
+    convert keyword argument names (which cannot contain dashes) to HTML
+    attribute names, e.g. data-*.
+    """
+    
+    return {key.replace('_', '-'): kwargs[key] for key in kwargs.keys()}
+
+
+@register.inclusion_tag('djem/form_field.html')
+def form_field(field, extra_classes=None, **kwargs):
+    """
+    Render a form field, including the input, label, errorlist (if any), and
+    help text block (if any), within a wrapping element.
+    The wrapping element can be specified with ``DJEM_FORM_FIELD_TAG``. By
+    default it is a div.
+    It will have the CSS class "form-field", plus any classes defined by the
+    field itself, plus any extra classes passed in to the tag as the second
+    argument.
+    Keyword arguments to the tag are added to the wrapping element as HTML
+    attributes, with underscores (_) replaced with dashes (-) to allow
+    attributes such as data-*.
+    """
+    
+    # Convert underscores in keyword arguments to dashes, to support attributes
+    # such as data-*, and since dashes are not valid in Python keyword argument
+    # names
+    kwargs = _transform_kwargs(kwargs)
+    
+    context = {
+        'wrapper_tag': getattr(settings, 'DJEM_FORM_FIELD_TAG', DEFAULT_FORM_FIELD_TAG),
+        'field': field,
+        'extra_classes': extra_classes,
+        'kwargs': kwargs
+    }
+    
+    return context
+
+
+class CheckboxNode(Node):
+    
+    child_nodelists = ('nodelist',)
+    
+    def __init__(self, field, nodelist, extra_classes, kwargs):
+        
+        self.field = field
+        self.nodelist = nodelist
+        self.extra_classes = extra_classes
+        self.kwargs = kwargs
+    
+    def render(self, context):
+        
+        field = self.field.resolve(context)
+        label = self.nodelist.render(context)
+        
+        if not label.strip():
+            label = field.label
+        
+        extra_classes = self.extra_classes
+        if extra_classes:
+            extra_classes = self.extra_classes.resolve(context)
+        
+        kwargs = self.kwargs
+        if kwargs:
+            for k, v in self.kwargs.items():
+                kwargs[k] = v.resolve(context)
+            
+            # Convert underscores in keyword arguments to dashes, to support
+            # attributes such as data-*, and since dashes are not valid in
+            # Python keyword argument names
+            kwargs = _transform_kwargs(kwargs)
+        
+        return loader.render_to_string('djem/form_field.html', {
+            'wrapper_tag': getattr(settings, 'DJEM_FORM_FIELD_TAG', DEFAULT_FORM_FIELD_TAG),
+            'field': field,
+            'extra_classes': extra_classes,
+            'kwargs': kwargs,
+            'checkbox_label': label
+        })
+
+
+@register.tag
+def checkbox(parser, token):
+    """
+    Render a checkbox form field, including the input, label, errorlist (if any),
+    and help text block (if any), within a wrapping element.
+    Arguments and options as per the ``form_field`` tag.
+    Unlike ``form_field``, this is a block tag whose content becomes the label
+    for the checkbox, replacing the label specified by the field. This label is
+    displayed *after* the checkbox input, as opposed to before.
+    This tag allows the specification of labels that would otherwise not be
+    possible, e.g. including links.
+    E.g.
+        {% checkbox form.terms_checkbox %}
+            I agree to the <a href="{% url 'terms' %}" target="_blank">Terms of service</a>
+        {% endcheckbox %}
+    """
+    
+    bits = list(token.split_contents())
+    if len(bits) < 2:
+        raise TemplateSyntaxError('{0!r} takes at least one argument'.format(bits[0]))
+    
+    nodelist = parser.parse(('endcheckbox',))
+    parser.next_token()  # consume {{ endcheckbox }}
+    
+    field = parser.compile_filter(bits[1])
+    
+    try:
+        extra_classes = bits[2]
+    except IndexError:
+        extra_classes = None
+        kwargs = None
+    else:
+        if '=' in extra_classes:
+            extra_classes = None
+            kwargs = bits[2:]
+        else:
+            extra_classes = parser.compile_filter(extra_classes)
+            kwargs = bits[3:]
+        
+        kwargs = token_kwargs(kwargs, parser)
+    
+    return CheckboxNode(field, nodelist, extra_classes, kwargs)
--- djem/tests/test_templatetags.py
+++ djem/tests/test_templatetags.py
@@ -1,11 +1,18 @@
+import re
+
+from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Permission
 from django.core.paginator import Paginator
 from django.http import HttpResponse
 from django.template import TemplateDoesNotExist, TemplateSyntaxError, engines
-from django.test import RequestFactory, TestCase
+from django.test import RequestFactory, TestCase, override_settings
 
 from .models import CommonInfoTest
+
+
+BETWEEN_TAG_WHITESPACE_RE = re.compile(r'>\s+<')
+EXCESS_WHITESPACE_RE = re.compile(r'\s\s+')
 
 
 class TemplateRendererMixin(object):
@@ -13,14 +20,24 @@ class TemplateRendererMixin(object):
     A helper for the following test cases, which render templates they provide
     as strings (not paths to files).
     
-    Requires a ``user`` attribute be accessible on the class.
+    Adds "user" to the context if a ``user`` attribute is accessible on the class.
     """
     
-    def render_template(self, template_string, context):
+    def render_template(self, template_string, context, flatten=True):
         
-        context['user'] = self.user
+        try:
+            context['user'] = self.user
+        except AttributeError:
+            pass
         
         output = engines['django'].from_string(template_string).render(context)
+        
+        if flatten:
+            # Remove whitespace between tags
+            output = re.sub(BETWEEN_TAG_WHITESPACE_RE, '><', output)
+            
+            # Replace instances of multiple whitespace characters with a single space
+            output = re.sub(EXCESS_WHITESPACE_RE, ' ', output)
         
         return output.strip()  # remove unnecessary whitespace
 
@@ -956,3 +973,499 @@ class PaginateTestCase(TemplateRendererMixin, TestCase):
         
         self.assertNotIn('Next', output)
         self.assertNotIn('Last', output)
+
+
+class FormFieldTestCase(TemplateRendererMixin, TestCase):
+    
+    @classmethod
+    def setUpTestData(cls):
+        
+        class TestForm(forms.Form):
+            
+            required_css_class = 'form-field--required'
+            
+            field = forms.CharField(required=False, label='Test Field')
+            required_field = forms.CharField()
+            help_field = forms.CharField(help_text='Some helpful text.')
+        
+        cls.form = TestForm
+    
+    def test_no_argument(self):
+        """
+        Test the form_field template tag when not provided with an argument. It
+        should raise a TemplateSyntaxError - at least one argument is required.
+        """
+        
+        template_string = (
+            '{% load djem %}'
+            '{% form_field %}'
+        )
+        
+        with self.assertRaises(TemplateSyntaxError):
+            self.render_template(template_string, {})
+    
+    def test_default_wrapper(self):
+        """
+        Test the form_field template tag when no custom ``DJEM_FORM_FIELD_TAG``
+        setting has been specified. It should use a <div> as the wrapping
+        element.
+        """
+        
+        template_string = (
+            '{% load djem %}'
+            '{% form_field form.field %}'
+        )
+        
+        output = self.render_template(template_string, {
+            'form': self.form()
+        })
+        
+        self.assertEqual(output[:25], '<div class="form-field" >')
+        self.assertEqual(output[-6:], '</div>')
+    
+    @override_settings(DJEM_FORM_FIELD_TAG='li')
+    def test_custom_wrapper(self):
+        """
+        Test the form_field template tag when a custom ``DJEM_FORM_FIELD_TAG``
+        setting has been specified. It should use the specified tag as the
+        wrapping element.
+        """
+        
+        template_string = (
+            '{% load djem %}'
+            '{% form_field form.field %}'
+        )
+        
+        output = self.render_template(template_string, {
+            'form': self.form()
+        })
+        
+        self.assertEqual(output[:24], '<li class="form-field" >')
+        self.assertEqual(output[-5:], '</li>')
+    
+    def test_content(self):
+        """
+        Test the form_field template tag in basic usage. It should output all
+        expected HTML.
+        """
+        
+        template_string = (
+            '{% load djem %}'
+            '{% form_field form.field %}'
+        )
+        
+        output = self.render_template(template_string, {
+            'form': self.form()
+        })
+        
+        self.assertEqual(
+            output,
+            '<div class="form-field" ><label for="id_field">Test Field:</label><input type="text" name="field" id="id_field" /></div>'
+        )
+    
+    def test_errors(self):
+        """
+        Test the form_field template tag when used with a field that contains
+        errors after a form submission. It should add an element containing the
+        list of errors.
+        """
+        
+        template_string = (
+            '{% load djem %}'
+            '{% form_field form.required_field %}'
+        )
+        
+        output = self.render_template(template_string, {
+            'form': self.form()
+        })
+        
+        self.assertNotIn('<ul class="errors">', output)
+        
+        template_string = (
+            '{% load djem %}'
+            '{% form_field form.required_field %}'
+        )
+        
+        output = self.render_template(template_string, {
+            'form': self.form({})  # bind form
+        })
+        
+        self.assertIn('<ul class="errorlist"><li>This field is required.</li></ul>', output)
+    
+    def test_help_text(self):
+        """
+        Test the form_field template tag when used with a field that defines
+        help text. It should add an element containing the help text.
+        """
+        
+        form = self.form()
+        template_string = (
+            '{% load djem %}'
+            '{% form_field form.field %}'
+        )
+        
+        output = self.render_template(template_string, {
+            'form': form
+        })
+        
+        self.assertNotIn('<div class="form-field__help">', output)
+        
+        template_string = (
+            '{% load djem %}'
+            '{% form_field form.help_field %}'
+        )
+        
+        output = self.render_template(template_string, {
+            'form': form
+        })
+        
+        self.assertIn('<div class="form-field__help">Some helpful text.</div>', output)
+    
+    def test_css_classes(self):
+        """
+        Test the form_field template tag when used with a field that defines
+        css classes. It should add those classes to the wrapping element.
+        """
+        
+        template_string = (
+            '{% load djem %}'
+            '{% form_field form.required_field %}'
+        )
+        
+        output = self.render_template(template_string, {
+            'form': self.form()
+        })
+        
+        self.assertIn('class="form-field form-field--required"', output)
+    
+    def test_extra_classes__string(self):
+        """
+        Test the form_field template tag when the ``extra_classes`` argument is
+        given as a string. It should include those classes on the wrapping
+        element.
+        """
+        
+        template_string = (
+            '{% load djem %}'
+            '{% form_field form.field "testclass1 testclass2" %}'
+        )
+        
+        output = self.render_template(template_string, {
+            'form': self.form()
+        })
+        
+        self.assertIn('class="form-field testclass1 testclass2"', output)
+    
+    def test_extra_classes__variable(self):
+        """
+        Test the form_field template tag when the ``extra_classes`` argument is
+        given as a template context variable. It should include those classes
+        on the wrapping element.
+        """
+        
+        template_string = (
+            '{% load djem %}'
+            '{% form_field form.required_field classes %}'
+        )
+        
+        output = self.render_template(template_string, {
+            'form': self.form(),
+            'classes': 'testclass1 testclass2'
+        })
+        
+        self.assertIn('class="form-field form-field--required testclass1 testclass2"', output)
+    
+    def test_extra_attributes__strings(self):
+        """
+        Test the form_field template tag when keyword arguments are given with
+        values as strings. It should include those arguments as HTML attributes
+        on the wrapping element, replacing underscores with dashes as necessary.
+        """
+        
+        template_string = (
+            '{% load djem %}'
+            '{% form_field form.field style="color: red;" data_id="1234" %}'
+        )
+        
+        output = self.render_template(template_string, {
+            'form': self.form()
+        })
+        
+        self.assertIn('<div class="form-field" style="color: red;" data-id="1234" >', output)
+    
+    def test_extra_attributes__variables(self):
+        """
+        Test the form_field template tag when keyword arguments are given with
+        values as template context variables. It should include those arguments
+        as HTML attributes on the wrapping element, replacing underscores with
+        dashes as necessary.
+        """
+        
+        template_string = (
+            '{% load djem %}'
+            '{% form_field form.field style=style data_id=id %}'
+        )
+        
+        output = self.render_template(template_string, {
+            'form': self.form(),
+            'style': 'color: red;',
+            'id': '1234'
+        })
+        
+        self.assertIn('<div class="form-field" style="color: red;" data-id="1234" >', output)
+
+
+class CheckboxTestCase(TemplateRendererMixin, TestCase):
+    
+    @classmethod
+    def setUpTestData(cls):
+        
+        class TestForm(forms.Form):
+            
+            required_css_class = 'form-field--required'
+            
+            field = forms.BooleanField(required=False, label='Test Field')
+            required_field = forms.BooleanField()
+            help_field = forms.BooleanField(help_text='Some helpful text.')
+        
+        cls.form = TestForm
+    
+    def test_no_argument(self):
+        """
+        Test the checkbox template tag when not provided with an argument. It
+        should raise a TemplateSyntaxError - at least one argument is required.
+        """
+        
+        template_string = (
+            '{% load djem %}'
+            '{% checkbox %}{% endcheckbox%}'
+        )
+        
+        with self.assertRaisesMessage(TemplateSyntaxError, "'checkbox' takes at least one argument"):
+            self.render_template(template_string, {})
+    
+    def test_no_end_tag(self):
+        """
+        Test the checkbox template tag when not provided with an argument. It
+        should raise a TemplateSyntaxError - at least one argument is required.
+        """
+        
+        template_string = (
+            '{% load djem %}'
+            '{% checkbox form.field %}'
+        )
+        
+        with self.assertRaisesMessage(TemplateSyntaxError, 'Unclosed tag'):
+            self.render_template(template_string, {
+                'form': self.form()
+            })
+    
+    def test_default_wrapper(self):
+        """
+        Test the checkbox template tag when no custom ``DJEM_FORM_FIELD_TAG``
+        setting has been specified. It should use a <div> as the wrapping
+        element.
+        """
+        
+        template_string = (
+            '{% load djem %}'
+            '{% checkbox form.field %}Click this{% endcheckbox%}'
+        )
+        
+        output = self.render_template(template_string, {
+            'form': self.form()
+        })
+        
+        self.assertEqual(output[:25], '<div class="form-field" >')
+        self.assertEqual(output[-6:], '</div>')
+    
+    @override_settings(DJEM_FORM_FIELD_TAG='li')
+    def test_custom_wrapper(self):
+        """
+        Test the checkbox template tag when a custom ``DJEM_FORM_FIELD_TAG``
+        setting has been specified. It should use the specified tag as the
+        wrapping element.
+        """
+        
+        template_string = (
+            '{% load djem %}'
+            '{% checkbox form.field %}Click this{% endcheckbox%}'
+        )
+        
+        output = self.render_template(template_string, {
+            'form': self.form()
+        })
+        
+        self.assertEqual(output[:24], '<li class="form-field" >')
+        self.assertEqual(output[-5:], '</li>')
+    
+    def test_content(self):
+        """
+        Test the checkbox template tag in basic usage. It should output all
+        expected HTML.
+        """
+        
+        template_string = (
+            '{% load djem %}'
+            '{% checkbox form.field %}Click this{% endcheckbox%}'
+        )
+        
+        output = self.render_template(template_string, {
+            'form': self.form()
+        })
+        
+        self.assertEqual(
+            output,
+            '<div class="form-field" ><input type="checkbox" name="field" id="id_field" /><label class="check-label" for="id_field"> Click this </label></div>'
+        )
+    
+    def test_errors(self):
+        """
+        Test the checkbox template tag when used with a field that contains
+        errors after a form submission. It should add an element containing the
+        list of errors.
+        """
+        
+        template_string = (
+            '{% load djem %}'
+            '{% checkbox form.required_field %}Click this{% endcheckbox%}'
+        )
+        
+        output = self.render_template(template_string, {
+            'form': self.form()
+        })
+        
+        self.assertNotIn('<ul class="errors">', output)
+        
+        template_string = (
+            '{% load djem %}'
+            '{% checkbox form.required_field %}Click this{% endcheckbox%}'
+        )
+        
+        output = self.render_template(template_string, {
+            'form': self.form({})  # bind form
+        })
+        
+        self.assertIn('<ul class="errorlist"><li>This field is required.</li></ul>', output)
+    
+    def test_help_text(self):
+        """
+        Test the checkbox template tag when used with a field that defines
+        help text. It should add an element containing the help text.
+        """
+        
+        form = self.form()
+        template_string = (
+            '{% load djem %}'
+            '{% checkbox form.field %}Click this{% endcheckbox%}'
+        )
+        
+        output = self.render_template(template_string, {
+            'form': form
+        })
+        
+        self.assertNotIn('<div class="form-field__help">', output)
+        
+        template_string = (
+            '{% load djem %}'
+            '{% checkbox form.help_field %}Click this{% endcheckbox%}'
+        )
+        
+        output = self.render_template(template_string, {
+            'form': form
+        })
+        
+        self.assertIn('<div class="form-field__help">Some helpful text.</div>', output)
+    
+    def test_css_classes(self):
+        """
+        Test the checkbox template tag when used with a field that defines
+        css classes. It should add those classes to the wrapping element.
+        """
+        
+        template_string = (
+            '{% load djem %}'
+            '{% checkbox form.required_field %}Click this{% endcheckbox%}'
+        )
+        
+        output = self.render_template(template_string, {
+            'form': self.form()
+        })
+        
+        self.assertIn('class="form-field form-field--required"', output)
+    
+    def test_extra_classes__string(self):
+        """
+        Test the checkbox template tag when the ``extra_classes`` argument is
+        given as a string. It should include those classes on the wrapping
+        element.
+        """
+        
+        template_string = (
+            '{% load djem %}'
+            '{% checkbox form.field "testclass1 testclass2" %}Click this{% endcheckbox%}'
+        )
+        
+        output = self.render_template(template_string, {
+            'form': self.form()
+        })
+        
+        self.assertIn('class="form-field testclass1 testclass2"', output)
+    
+    def test_extra_classes__variable(self):
+        """
+        Test the checkbox template tag when the ``extra_classes`` argument is
+        given as a template context variable. It should include those classes
+        on the wrapping element.
+        """
+        
+        template_string = (
+            '{% load djem %}'
+            '{% checkbox form.required_field classes %}Click this{% endcheckbox%}'
+        )
+        
+        output = self.render_template(template_string, {
+            'form': self.form(),
+            'classes': 'testclass1 testclass2'
+        })
+        
+        self.assertIn('class="form-field form-field--required testclass1 testclass2"', output)
+    
+    def test_extra_attributes__strings(self):
+        """
+        Test the checkbox template tag when keyword arguments are given with
+        values as strings. It should include those arguments as HTML attributes
+        on the wrapping element, replacing underscores with dashes as necessary.
+        """
+        
+        template_string = (
+            '{% load djem %}'
+            '{% checkbox form.field style="color: red;" data_id="1234" %}Click this{% endcheckbox%}'
+        )
+        
+        output = self.render_template(template_string, {
+            'form': self.form()
+        })
+        
+        self.assertIn('<div class="form-field" style="color: red;" data-id="1234" >', output)
+    
+    def test_extra_attributes__variables(self):
+        """
+        Test the checkbox template tag when keyword arguments are given with
+        values as template context variables. It should include those arguments
+        as HTML attributes on the wrapping element, replacing underscores with
+        dashes as necessary.
+        """
+        
+        template_string = (
+            '{% load djem %}'
+            '{% checkbox form.field style=style data_id=id %}Click this{% endcheckbox%}'
+        )
+        
+        output = self.render_template(template_string, {
+            'form': self.form(),
+            'style': 'color: red;',
+            'id': '1234'
+        })
+        
+        self.assertIn('<div class="form-field" style="color: red;" data-id="1234" >', output)
