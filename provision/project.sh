0740b3c800452228b91407f60862e53f31ff6215
Alex Church <alex@church.id.au>
2019-09-18 21:02:17 +1000

    Updated/fixed testing environments.

---
+++ provision/project.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+source /tmp/settings.sh
+
+# Required for running tests against older versions of Python
+apt-get -qq install python3-distutils
--- provision/versions.sh
+++ provision/versions.sh
@@ -3,14 +3,14 @@
 
 # The Python version to use for the project virtualenv. Leave blank to use the
 # system version.
-# Define an explicit version: BASE_PYTHON_VERSION='3.6.4'
-BASE_PYTHON_VERSION='3.7.1'
+# Define an explicit version: BASE_PYTHON_VERSION='3.7.4'
+BASE_PYTHON_VERSION='3.7.4'
 
 # List of additional Python versions to install (e.g. test with multiple
 # versions via tox). If not included in the list, BASE_PYTHON_VERSION will be
 # appended to the end.
-# Define explicit versions: PYTHON_VERSIONS=('2.7.14' '3.5.4' '3.6.4')
-PYTHON_VERSIONS=('2.7.15'  '3.4.9'  '3.5.6'  '3.6.7')
+# Define explicit versions: PYTHON_VERSIONS=('2.7.16' '3.5.7' '3.6.9')
+PYTHON_VERSIONS=('2.7.16'  '3.5.7'  '3.6.9')
 
 # Node.js version (major version only - the latest minor version will always be used)
 NODE_VERSION='8'
