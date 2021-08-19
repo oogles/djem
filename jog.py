ad111f8530e018a6ad9f6e70246b83ad0f43bc5a
Alex Church <alex@church.id.au>
2021-08-20 09:21:46 +1000

    Added jogger release task.

--- jog.py
+++ jog.py
@@ -1,7 +1,9 @@
 from jogger.tasks import DocsTask, LintTask, TestTask
+from jogger.tasks._release import ReleaseTask
 
 tasks = {
     'docs': DocsTask,
     'lint': LintTask,
-    'test': TestTask
+    'test': TestTask,
+    'release': ReleaseTask
 }
