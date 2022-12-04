ef0985e943cc8f3bae8b1a1850eb10cc3c1f7e7b
Alex Church <alex@church.id.au>
2022-12-04 16:56:09 +1100

    Added jogger shortcuts to common Docker commands.

--- jog.py
+++ jog.py
@@ -1,7 +1,28 @@
 from jogger.tasks import DocsTask, LintTask, TestTask
 from jogger.tasks._release import ReleaseTask
 
+
+def docker_down(settings, stdout, stderr):
+    """
+    Stop and remove the Docker containers for local development.
+    """
+    
+    answer = input('Destroy docker containers? [Y/n] ')
+    
+    if answer.lower() != 'y':
+        return
+    
+    return 'docker compose down --remove-orphans'
+
+
 tasks = {
+    # Docker tasks. Run on the host to control Docker.
+    'up': 'docker compose up --wait',
+    'stop': 'docker compose stop',
+    'down': docker_down,
+    'ssh': 'docker exec -it djem /bin/bash',
+    
+    # Dev tasks. Run on the guest to aid development.
     'docs': DocsTask,
     'lint': LintTask,
     'test': TestTask,
