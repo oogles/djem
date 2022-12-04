89e9e55f80243faf29d6f87e9ef61e8120d007e7
Alex Church <alex@church.id.au>
2022-12-04 16:55:33 +1100

    Removed Vagrant.

--- provision/project.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-source /tmp/settings.sh
-
-# Required for running tests against older versions of Python
-apt-get -qq install python3-distutils
--- provision/scripts/cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo " "
-echo " --- Clean up ---"
-
-# Remove the temporary files created by init.sh
-rm /tmp/settings.sh
-rm -rf /tmp/conf/
-echo "Done"
