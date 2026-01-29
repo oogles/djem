3815c89b8f3d937829419eca3da75f560426f9bd
Alex Church <alex@church.id.au>
2026-01-29 13:55:59 +1100

    Fixed linter warnings.

--- djem/tests/__init__.py
+++ djem/tests/__init__.py
@@ -2,7 +2,7 @@ from djem.utils.tests import setup_test_app
 
 # Use an explicit app label so as not to interfere with the tests of
 # setup_test_app() itself (which test implicit app labels)
-setup_test_app(__package__, 'djemtest')
+setup_test_app(__package__, 'djemtest')  # noqa: RUF067
 
 # Target for ROOT_URLCONF, since it has to point somewhere
-urlpatterns = []
+urlpatterns = []  # noqa: RUF067
